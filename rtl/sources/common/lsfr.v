d51fef907913be79f7f803014c67581b3a00f35a
Noah Harman <noahharman190@gmail.com>
2026-02-09 23:35:37 -0500

    feat: add food collision detection, LFSR respawn, and snake head wiring

--- rtl/sources/common/lsfr.v
+++ rtl/sources/common/lsfr.v
@@ -10,8 +10,8 @@ module lsfr (
             o_x_data <= 4'h1;
             o_y_data <= 4'h1;
         end else begin
-            o_x_data <= {o_x_data[2], o_x_data[1], o_x_data[0], o_x_data[3] ^ o_x_data[3]}; //Generates psuedo random bits to represent next food location
-            o_y_data <= {o_y_data[2], o_y_data[1], o_y_data[0], o_y_data[3] ^ o_y_data[2]};
+            o_x_data <= {o_x_data[2], o_x_data[1], o_x_data[0], o_x_data[3] ^ o_x_data[2]}; //Generates psuedo random bits to represent next food location
+            o_y_data <= {o_y_data[2], o_y_data[1], o_y_data[0], o_y_data[3] ^ o_y_data[0]};
         end
     end
 
